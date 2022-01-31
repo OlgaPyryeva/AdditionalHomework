d51c95fd809d0a96dc8c0845940d990a10d88bd4
Olga Pyryeva <vashkevicholga@gmail.com>
2022-02-01 00:19:51 +0300

    add(script.js):add home work lesson03

--- script.js
+++ script.js
@@ -1,21 +1,121 @@
-let num = 266219;
+let lang = prompt("Выберите язык дней недели", " en  или  ru");
+let weekDays = "";
+let n = +prompt("введите номер дня недели", "от 1 до 7");
 
-//console.log(typeof num); тип число
+//Решить через
+//a) через if,
 
-num = String(num);
-
-//console.log(typeof num); тип стал строка
-//console.log(num.length); длинна равна 6
-//console.log(num[0]); результат 2, получили первое число
-let result = 1;
-
-for (let i = 0; i < num.length; i++) {
-  result *= num[i];
+if (n == 1 && lang == "ru") {
+  weekDays = "понедельник";
+} else if (n == 1 && lang == "en") {
+  weekDays = "monday";
+} else if (n == 2 && lang == "ru") {
+  weekDays = "вторник";
+} else if (n == 2 && lang == "en") {
+  weekDays = "tuesday";
+} else if (n == 3 && lang == "ru") {
+  weekDays = "среда";
+} else if (n == 3 && lang == "en") {
+  weekDays = "wednesday";
+} else if (n == 4 && lang == "ru") {
+  weekDays = "четверг";
+} else if (n == 4 && lang == "en") {
+  weekDays = "thursday";
+} else if (n == 5 && lang == "ru") {
+  weekDays = "пятница";
+} else if (n == 5 && lang == "en") {
+  weekDays = "friday";
+} else if (n == 6 && lang == "ru") {
+  weekDays = "суббота";
+} else if (n == 6 && lang == "en") {
+  weekDays = "saturday";
+} else if (n == 7 && lang == "ru") {
+  weekDays = "воскресенье";
+} else if (n == 7 && lang == "en") {
+  weekDays = "sunday";
+} else {
+  weekDays = "в неделе всего 7 дней / error";
 }
+console.log("Через иф " + weekDays);
 
-console.log(result); // ч.2 дз - получили произведение чисел из которых состоит переменная num
+// b) через switch-case
 
-result **= 3; // ч.3 дз возвели в степень 3
-//console.log(typeof result); тип число
-result = String(result);
-console.log(result.substr(0, 2)); // ч.4 дз - первые 2 цифры после возведения в степень
+if (lang == "ru") {
+  switch (n) {
+    case 1:
+      weekDays = "Понедельник";
+      break;
+    case 2:
+      weekDays = "Вторник";
+      break;
+    case 3:
+      weekDays = "Среда";
+      break;
+    case 4:
+      weekDays = "Четверг";
+      break;
+    case 5:
+      weekDays = "Пятница";
+      break;
+    case 6:
+      weekDays = "Суббота";
+      break;
+    case 7:
+      weekDays = "Воскресенье";
+      break;
+    default:
+      weekDays = "Неверные данные";
+  }
+} else if (lang == "en") {
+  switch (n) {
+    case 1:
+      weekDays = "Monday";
+      break;
+    case 2:
+      weekDays = "Tuesday";
+      break;
+    case 3:
+      weekDays = "Wednesday";
+      break;
+    case 4:
+      weekDays = "Thursday";
+      break;
+    case 5:
+      weekDays = "Friday";
+      break;
+    case 6:
+      weekDays = "Saturday";
+      break;
+    case 7:
+      weekDays = "Sunday";
+      break;
+    default:
+      weekDays = "Wrong data";
+  }
+}
+console.log("через switch-case " + weekDays);
+
+//c) через многомерный массив без ифов и switch.
+let days = [
+  ["Понедельник", "Monday"],
+  ["Вторник", "Tuesday"],
+  ["Среда", "Wednesday"],
+  ["Четверг", "Thursday"],
+  ["Пятница", "Friday"],
+  ["Суббота", "Saturday"],
+  ["Воскресенье", "Sunday"],
+];
+
+lang == "ru"
+  ? console.log(days[n - 1][0])
+  : lang == "en"
+  ? console.log(days[n - 1][1])
+  : console.log("не правильно выбран язык");
+
+//п.2 усложненного домашнего задания
+let namePerson = prompt("Введите имя");
+namePerson == "Артем"
+  ? console.log("директор")
+  : namePerson == "Александр"
+  ? console.log("преподаватель")
+  : console.log("студент");

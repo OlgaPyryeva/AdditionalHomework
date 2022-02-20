a846f5528fdc17d1848f698537c17bde586c01b3
Olga Pyryeva <vashkevicholga@gmail.com>
2022-02-20 10:57:03 +0300

    add(script.js, index.html): add homework lesson16

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.backgroundColor = bg;
-  this.fontSize = fontSize;
-};
+"use strict";
 
-DomElement.prototype.createElemMethod = function (text) {
-  if (this.selector[0] === ".") {
-    const newDiv = document.createElement("div");
-    newDiv.className = this.selector.slice(1);
-    newDiv.textContent = text;
-    newDiv.style.cssText = `
-       height:${this.height}px;
-       width:${this.width}px;
-       background-color:${this.backgroundColor};
-       font-size:${this.fontSize}px`;
-
-    document.body.prepend(newDiv);
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
   }
-  if (this.selector[0] === "#") {
-    const newP = document.createElement("p");
-    newP.id = "second";
-    newP.textContent = "новый параграф";
-    newP.style.cssText = `
-       height:${this.height}px;
-       width:${this.width}px;
-       background-color:${this.backgroundColor};
-       font-size:${this.fontSize}px`;
-
-    document.body.prepend(newP);
+}
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
   }
-};
+}
 
-const domElem = new DomElement(".block", 10, 10, "red", 10);
-domElem.createElemMethod("текст");
+const log = new Second();
+log.hello();

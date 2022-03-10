2622f79b891714e197ae20cdb68d2ee12b15b367
Olga Pyryeva <vashkevicholga@gmail.com>
2022-03-10 08:57:37 +0300

    add(script.js): add new variable newData

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let newData;
+
 //1) Написать две функции для реализации AJAX запросов: getData и sendData
 
 const getData = (url) => {
@@ -20,15 +22,19 @@ const sendData = (url, data) => {
 //3) Реализовать получение данных из файла .json (прикреплен под видео)  через функцию getData
 //http://127.0.0.1:5501/db.json
 
-getData("db.json").then((data) => console.log(data));
+getData("db.json")
+  .then((data) => {
+    newData = JSON.stringify(data);
+  })
+  .then((data) => console.log(newData));
 
 //4) После получения объекта из файла .json должна произойти отправка данных (которые мы получили из файла .json)
 //на URL через функцию sendData
 
 //https://jsonplaceholder.typicode.com/posts
 
-sendData("https://jsonplaceholder.typicode.com/posts", JSON.stringify("data"))
-  .then((data) => console.log(data))
+sendData("https://jsonplaceholder.typicode.com/posts", newData)
+  .then((newData) => console.log(newData))
   .catch((error) => console.log(error));
 
 // 5) Ошибки должны быть обработаны
